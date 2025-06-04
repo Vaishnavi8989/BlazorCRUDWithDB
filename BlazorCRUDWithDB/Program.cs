1c05fc22cbd54fcf06fe5457db9446a286ec746f
Vaishnavi Pawar <Vaishnavi.Pawar@sadafco.com>
2025-06-04 17:30:14 +0530

    Add project files.

---
+++ BlazorCRUDWithDB/Program.cs
@@ -0,0 +1,33 @@
+using BlazorCRUDWithDB.Data;
+using BlazorCRUDWithDB.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<EmployeeService>();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ BlazorCRUDWithDB/Services/EmployeeService.cs
@@ -0,0 +1,53 @@
+﻿using BlazorCRUDWithDB.Data;
+using BlazorCRUDWithDB.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorCRUDWithDB.Services
+{
+    public class EmployeeService
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Employee>> GetAllAsync() => await _context.Employees.ToListAsync();
+
+        public async Task<Employee> GetByIdAsync(int id) => await _context.Employees.FindAsync(id);
+
+        public async Task AddAsync(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee != null)
+            {
+                _context.Employees.Remove(employee);
+                await _context.SaveChangesAsync();
+            }
+        }
+        public async Task<bool> EmployeeExistsAsync(string name)
+        {
+            return await _context.Employees.AnyAsync(e => e.Name == name);
+        }
+        //public async Task<bool> EmployeeEmailExistsAsync(string email)
+        //{
+        //    return await _context.Employees.AnyAsync(e => e.Email == email);
+        //}
+
+    }
+}
